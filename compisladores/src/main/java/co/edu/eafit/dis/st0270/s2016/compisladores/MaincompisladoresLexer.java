f0035b3d206efac1854f755c54a64d35e82e92b9
acordob5 <acordob5@597b656a-1e46-4e4b-ad7b-eba3e787667f>
2016-04-30 19:03:17 +0000

    escribiendo el main

--- compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/MaincompisladoresLexer.java
+++ compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/MaincompisladoresLexer.java
@@ -1,13 +1,33 @@
 package co.edu.eafit.dis.st0270.s2016.compisladores;
 
+import co.edu.eafit.dis.st0270.s2016.compisladores.lexer.SCcompisladores.java;
+import import co.edu.eafit.dis.st0270.s2016.sisctr.token.SCToken;
+
 /**
  * Hello world!
  *
  */
 public class MaincompisladoresLexer 
 {
+
+    private String ficheros[];
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	if(args.length > 0){
+	    
+	    if(args[0] == "jflex"){
+		//llamo jflex
+	    }else if(args[0] == "antlr"){
+		//
+	    }else if(args[0] == "both"){
+		
+	    }
+	    
+	}
+    }
+
+    public callJflex(){
+
     }
 }
