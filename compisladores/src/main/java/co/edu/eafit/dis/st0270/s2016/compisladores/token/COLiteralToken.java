e972326bcfd6d61962ea653951b93ba76ae62763
esilvac <esilvac@597b656a-1e46-4e4b-ad7b-eba3e787667f>
2016-05-02 13:47:10 +0000

    correciones

--- compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/token/COLiteralToken.java
+++ compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/token/COLiteralToken.java
@@ -5,16 +5,24 @@ package co.edu.eafit.dis.st0270.s2016.compisladores.token;
 
 public class COLiteralToken
     extends co.edu.eafit.dis.st0270.s2016.sisctr.token.LiteralToken {
-    
+    private int valor;
+
     public COLiteralToken(int column, int line, String text) {
         super(column, line, text);
+	this.valor = valor;
     }
     
     public String toString(){
 	String column = "" + super.getColumn();
 	String line = "" + super.getLine();
-	String id = super.getText();    
-	return "Literal: " + id + " linea: " + line + " columna: " + column;
+	String id = super.getText(); 
+	int valor = Integer.parseInt(id);
+	if(valor <= 32767 && valor >= -32767){   
+	    return "Literal: " + id + " linea: " + line + " columna: " + column;
+	} 
+	else {
+	    return "Exception: Literal no reconocido: " + id + " linea: " + line + " columna: " + column ;
+	}
     }
 }
 
--- compisladores/src/main/jflex/SCcompisladoresJFlex.jflex
+++ compisladores/src/main/jflex/SCcompisladoresJFlex.jflex
@@ -51,7 +51,7 @@ SeparatorWord = ","
 
 SpecialSymbol = "()"
 
-LiteralToken = 0 | ([1-9][0-9][0-9][0-9])*
+LiteralToken = 0 | [1-9][0-9]*
 
 IDToken = ([a-z]|[A-Z])*
 
