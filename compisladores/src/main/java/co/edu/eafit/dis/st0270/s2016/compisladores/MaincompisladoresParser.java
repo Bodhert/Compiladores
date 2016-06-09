11dd4c3db9b4b58df6a3032e1fe73287fadc11a3
acordob5 <acordob5@597b656a-1e46-4e4b-ad7b-eba3e787667f>
2016-06-09 05:24:06 +0000

    funciona

--- compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/MaincompisladoresParser.java
+++ compisladores/src/main/java/co/edu/eafit/dis/st0270/s2016/compisladores/MaincompisladoresParser.java
@@ -9,47 +9,44 @@ import java.io.File;
 import java.util.Set;
 import co.edu.eafit.dis.st0270.s2016.compisladores.parser.*;
 import java.util.HashSet;
+import java.io.FileNotFoundException;
 
 public class MaincompisladoresParser{
 
   public static void main(String args[]) throws Exception{
-    // try(){
-    //
-    // }catch(){
-    //
-    // }
-    for(int i = 0; i < args.length; ++i){
-      String inputFile = args[i];
-      InputStream is = new FileInputStream(inputFile);
-      ANTLRInputStream input = new ANTLRInputStream(is);
-      SCcompisladoresANTLRParserLexer lexer = new SCcompisladoresANTLRParserLexer (input);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      SCcompisladoresANTLRParserParser parser = new SCcompisladoresANTLRParserParser(tokens);
-      ParseTree tree = parser.control();
-      SCcompisladoresANTLRDefVisitor def = new SCcompisladoresANTLRDefVisitor();
-      Set<String> usedVars = def.visit(tree);
+    try{
+      for(int i = 0; i < args.length; ++i){
+        String inputFile = args[i];
+        InputStream is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        SCcompisladoresANTLRParserLexer lexer = new SCcompisladoresANTLRParserLexer (input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SCcompisladoresANTLRParserParser parser = new SCcompisladoresANTLRParserParser(tokens);
+        ParseTree tree = parser.control();
+        SCcompisladoresANTLRDefVisitor def = new SCcompisladoresANTLRDefVisitor();
+        Set<String> usedVars = def.visit(tree);
 
-      System.out.println("Fichero: " + inputFile);
+        System.out.println("Fichero: " + inputFile);
 
-      // for(String s: usedVars){
-      //   System.out.println("Variable bien definida: " + s);
-      // }
+        // for(String s: usedVars){
+        //   System.out.println("Variable bien definida: " + s);
+        // }
 
-      SCcompisladoresANTLRLetVisitor comp = new SCcompisladoresANTLRLetVisitor(usedVars);
-      Set<String> usedletVars = comp.visit(tree);
-      System.out.println("Bien Formado");
-      Set<String> badformed = comp.getBadvariables();
-      Set<String> intr = new HashSet<String>(usedletVars);
-      intr.retainAll(badformed);
+        SCcompisladoresANTLRLetVisitor comp = new SCcompisladoresANTLRLetVisitor(usedVars);
+        Set<String> usedletVars = comp.visit(tree);
+        System.out.println("Bien Formado");
+        Set<String> badformed = comp.getBadvariables();
+        Set<String> intr = new HashSet<String>(usedletVars);
+        intr.retainAll(badformed);
 
-      Set<String> well = comp.getDeclared();
-      Set<String> intrwell = new HashSet<String>(usedletVars);
-      intrwell.retainAll(well);
-      // for(String j: usedletVars){
-      //   System.out.println("let bien definida: " + j);
-      // }
+        Set<String> well = comp.getDeclared();
+        Set<String> intrwell = new HashSet<String>(usedletVars);
+        intrwell.retainAll(well);
+        // for(String j: usedletVars){
+        //   System.out.println("let bien definida: " + j);
+        // }
 
-      if(intr.size() > 0){
+        if(intr.size() > 0){
           System.out.println("Programa no valido");
           System.out.print("usa dipositivos: ");
           String result = "";
@@ -65,15 +62,20 @@ public class MaincompisladoresParser{
           }
           System.out.println(result.substring(0,result.length()-2));
 
-      }else{
-        System.out.println("Programa valido");
-        System.out.print("usa dipositivos: ");
-        String result = "";
-        for(String k: intrwell){
-          result +=  k +  ", ";
+        }else{
+          System.out.println("Programa valido");
+          System.out.print("usa dipositivos: ");
+          String result = "";
+          for(String k: intrwell){
+            result +=  k +  ", ";
+          }
+          System.out.println(result.substring(0,result.length()-2));
         }
-        System.out.println(result.substring(0,result.length()-2));
       }
+    }catch(FileNotFoundException e){
+      System.err.print(" No se encontro el fichero ");
+    }catch(Exception e){
+      System.err.print(" mal formado ");
     }
   }
 }
